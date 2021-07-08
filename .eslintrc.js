a9f62c9c87e47626531e0e8e913a5842ce8107e5
Angelo Perera <angeloperera@gmail.com>
2021-07-08 21:54:33 +1000

    Config updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,11 @@
 module.exports = {
-  root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "prettier/@typescript-eslint",
-    "jest",
+    "prettier/@typescript-eslint"
   ],
   env: {
     node: true,
